1c1aa56ba6acf7e5f918902298a507194ce36d40
vvpvvp <alias8516@sina.com.cn>
2017-05-21 21:13:54 +0800

    百度验证

--- .nuxt/App.vue
+++ .nuxt/App.vue
@@ -15,7 +15,7 @@ let layouts = {
 }
 
 export default {
-  head: {"titleTemplate":"%s - 诗词","meta":[{"charset":"utf-8"},{"name":"viewport","content":"width=device-width, initial-scale=1"},{"hid":"description","name":"诗词","content":"诗词, 诗歌, 唐诗三百首"}],"link":[{"rel":"icon","type":"image/x-icon","href":"/favicon.ico"}]},
+  head: {"titleTemplate":"%s - 诗词","meta":[{"charset":"utf-8"},{"name":"viewport","content":"width=device-width, initial-scale=1"},{"name":"description","content":"诗词, 诗歌, 唐诗三百首"},{"name":"baidu-site-verification","content":"77da2MvryU"}],"link":[{"rel":"icon","type":"image/x-icon","href":"/favicon.ico"}]},
   data: () => ({
     layout: null,
     layoutName: ''
--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,8 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: '诗词', content: '诗词, 诗歌, 唐诗三百首' }
+      { name: 'description', content: '诗词, 诗歌, 唐诗三百首' },
+      { name: 'baidu-site-verification', content: '77da2MvryU' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
