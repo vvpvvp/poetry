c1eae1e53e52cf4479837ffa07a607589579c41f
vvpvvp <alias8516@sina.com.cn>
2017-05-22 14:20:37 +0800

    build

--- pages/index.vue
+++ pages/index.vue
@@ -403,6 +403,10 @@
         <nuxt-link to="/search/?type=poetry&keyword=%e6%b8%b8%e4%bb%99">游仙</nuxt-link>
       </div>
     </div>
+    <footer class="comm-footer">
+      Copyright © 2017 <a href="http://www.ch-un.com" target="_blank">LAN</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <a href="http://www.miitbeian.gov.cn/" target="_blank" class="gray1">沪ICP备17010105号-1</a>
+    </footer>
   </div>
 </template>
 <script>
